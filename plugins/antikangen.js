052a7278290b655e489863609b4cd305f2092f20
Irwan X Yans <irwan081249@gmail.com>
2021-09-21 16:58:05 +0700

    bucin

---
+++ plugins/antikangen.js
@@ -0,0 +1,12 @@
+let fs = require('fs')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let zero = fs.readFileSync('./audio/iyaapasyg[1].mp3')
+conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+//conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*🔥WOAHHHHH 🔥*', 'status@broadcast')
+}
+
+handler.customPrefix = /^(kangen|tangen|kaaangen|kaangen|kange)?$/i
+handler.command = new RegExp
+
+module.exports = handler
