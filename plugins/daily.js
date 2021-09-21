fcb6d765061a781ac48c1bf4b213f1374428b077
Irwan X Yans <irwan081249@gmail.com>
2021-09-21 22:18:15 +0700

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix, isPrems }) => {
   let time = db.data.users[m.sender].lastclaim + 86400000
   if (new Date - db.data.users[m.sender].lastclaim < 86400000) throw `Kamu sudah mengklaim klaim harian hari ini\ntunggu selama ${msToTime(time - new Date())} lagi`
   db.data.users[m.sender].exp += isPrems ? prem * db.data.users[m.sender].level : free * db.data.users[m.sender].level
-  m.reply(`+${isPrems ? prem * db.data.users[m.sender].level : free * db.data.users[m.sender].level} XP\n\nsemakin tinggi level, semakin tinggi juga XP yang didapat`)
+  m.reply(`+${isPrems ? prem * db.data.users[m.sender].level : free * db.data.users[m.sender].level} Rupiah\n\nsemakin tinggi level, semakin tinggi juga Uang yang didapat`)
   db.data.users[m.sender].lastclaim = new Date * 1
 }
 handler.help = ['daily', 'claim']
