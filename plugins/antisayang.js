64a12dda2ea52014d36b09ba6ea5e7a119bd6c61
Irwan X Yans <irwan081249@gmail.com>
2021-09-21 16:51:58 +0700

    bucin dlus

---
+++ plugins/antisayang.js
@@ -0,0 +1,12 @@
+let fs = require('fs')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let zero = fs.readFileSync('./audio/halosyg.mp3')
+conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+//conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*🔥WOAHHHHH 🔥*', 'status@broadcast')
+}
+
+handler.customPrefix = /^(sayang|ayang|bebep|dear|beb|bebeb|saayang|syg|syng|aynk|syk|synk|synx|ayanx)?$/i
+handler.command = new RegExp
+
+module.exports = handler
