909d59bc6c5e90fe725651c620c36039f49b3727
Irwan X Yans <irwan081249@gmail.com>
2021-09-21 20:08:37 +0700

    Update antikangen.js

--- plugins/antikangen.js
+++ plugins/antikangen.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, participants }) => {
-let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+let users = participants.map((u) => u.jid);
 let zero = fs.readFileSync('./audio/kangen.mp3')
 conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true, contextInfo: { mentionedJid: users }})
 //conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*🔥WOAHHHHH 🔥*', 'status@broadcast')
