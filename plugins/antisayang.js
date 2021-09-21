6b4e5e43ada5582882b57f4f372e84a05679045f
Irwan X Yans <irwan081249@gmail.com>
2021-09-21 17:38:47 +0700

    biar ke tag

--- plugins/antisayang.js
+++ plugins/antisayang.js
@@ -1,13 +1,10 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, participants }) => {
-let users = participants.map((u) => u.jid);
+let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
 let zero = fs.readFileSync('./audio/halosyg.mp3')
-conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true, contextInfo: { mentionedJid: users }})
 //conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*🔥WOAHHHHH 🔥*', 'status@broadcast')
- contextInfo: {
-  mentionedJid: users
-        }
 }
 handler.customPrefix = /sayang|ayang|bebep|dear|beb|bebeb|saayang|syg|syng|aynk|syk|synk|synx|ayanx|ayank/i
 handler.command = new RegExp
