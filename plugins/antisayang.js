cafeaef7724a65b613c3fed1c1c6c5e9fac02953
Irwan X Yans <irwan081249@gmail.com>
2021-09-21 17:12:23 +0700

    Update antisayang.js

--- plugins/antisayang.js
+++ plugins/antisayang.js
@@ -5,8 +5,10 @@ let zero = fs.readFileSync('./audio/halosyg.mp3')
 conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 //conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*🔥WOAHHHHH 🔥*', 'status@broadcast')
 }
-
-handler.customPrefix = /sayang|ayang|bebep|dear|beb|bebeb|saayang|syg|syng|aynk|syk|synk|synx|ayanx/i
+contextInfo: {
+  mentionedJid: users
+        }
+handler.customPrefix = /sayang|ayang|bebep|dear|beb|bebeb|saayang|syg|syng|aynk|syk|synk|synx|ayanx|ayank/i
 handler.command = new RegExp
 
 module.exports = handler
