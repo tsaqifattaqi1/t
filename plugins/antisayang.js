501e6387bcf127c6139b242930a3028005932a9c
Irwan X Yans <irwan081249@gmail.com>
2021-09-21 17:02:44 +0700

    Update antisayang.js

--- plugins/antisayang.js
+++ plugins/antisayang.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-let zero = fs.readFileSync('./audio/halosyg[1].mp3')
+let zero = fs.readFileSync('./audio/halosyg.mp3')
 conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 //conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*🔥WOAHHHHH 🔥*', 'status@broadcast')
 }
