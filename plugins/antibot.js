5889e868cc1aab8604cb0e04f2d1431ff0dc30c0
Irwan X Yans <irwan081249@gmail.com>
2021-09-22 21:22:21 +0700

    Update antibot.js

--- plugins/antibot.js
+++ plugins/antibot.js
@@ -3,7 +3,7 @@ let handler = async (m) => {
 let stc = fs.readFileSync('./sticker/Bot.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Alo Tayang Acu*', 'status@broadcast')
 }
-handler.customPrefix = /bot|robot/i
+handler.customPrefix = /(bot|robot)?$/i
 handler.command = new RegExp
 
 module.exports = handler
