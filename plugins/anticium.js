a2699dfe93ea4cb36a451903bd2ec3585a9008ee
Irwan X Yans <irwan081249@gmail.com>
2021-09-22 21:09:06 +0700

    Update anticium.js

--- plugins/anticium.js
+++ plugins/anticium.js
@@ -1,12 +1,12 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn, participants }) => {
-let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+let handler = async (m) => {
 let zero = fs.readFileSync('./audio/mwah.mp3')
-conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true, contextInfo: { mentionedJid: users }})
+conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 //conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*🔥WOAHHHHH 🔥*', 'status@broadcast')
 }
-handler.customPrefix = /sun|mwah|mmwa|mwa|mwwa|mmmwa|cium|ciuuum|cciiuum|ciumm/i
+
+handler.customPrefix = /sun|cium|mwah|mmwah/i
 handler.command = new RegExp
 
 module.exports = handler
