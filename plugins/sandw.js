f3a6d2fdc91a00414cc359275be47fc2e4bf0f9a
Irwan X Yans <irwan081249@gmail.com>
2021-09-22 14:17:35 +0700

    Create sandw.js

---
+++ plugins/sandw.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('*[ ❗ ] Bentar kak...*')
+  let res = `https://api.zeks.xyz/api/sandw?apikey=apivinz&text=${response}`
+  conn.sendFile(m.chat, res, 'sandw.jpg', `Nih kak`, m, false)
+}
+handler.help = ['sandw'].map(v => v + ' <teks>')
+handler.tags = ['sticker', 'update']
+handler.command = /^(sandw)$/i
+
+module.exports = handler
