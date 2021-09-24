5f095dbbe4b01c8ea1e26a0e0e59a67995444d2e
Irwan X Yans <irwan081249@gmail.com>
2021-09-24 16:56:34 +0700

    Update premlist.js

--- plugins/premlist.js
+++ plugins/premlist.js
@@ -1,10 +1,29 @@
-let fs = require('fs')
-let handler = async (m, { conn, isOwner }) => {
-  let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-  conn.reply(m.chat, `╭━━•✵ ⃟  ⃟  ⃟✵•━━━━━━━━━━━━━╮\n┃╭┈─────────────⩵꙰ཱི࿐\n┃╰──Pengguna Premium!*──➤ ↶↷*` + `\n` + prem.map(v => isOwner ? '┃*〲 @' + v.replace(/@.+/, '') : '*〲 ' + conn.getName(v)).join`\n` + '\n╰━━━━━━━━━━━━━━━━━━━━╯', m, { contextInfo: { mentionedJid: prem } })
-}
-handler.help = ['premlist']
-handler.tags = ['owner', 'info']
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'User Premium')).buffer(), `
+╭━━•›ꪶ ۪۪⸙ ━ ━ ━ ━ ꪶ ۪۪⸙‹•━━╮ 
+┃╭┈─────────────⩵꙰ཱི࿐ 
+┃╰──*User Premium*──➤ ↶↷* 
+╰•͙✩̣̣
+⁙┃ ુོ 0888-2611-841 (Permanent) *OWNER*
+⁙┃ ુོ 0882-0059-91227 (Permanent)
+⁙┃ ુོ 0896-2248-8059 (Permanent)
+⁙┃ ુོ 0857-2573-6656 (Permanent)
+⁙┃ ુོ 0851-5733-6614 (Permanent)
+⁙┃ ુོ 0838-4407-0932 (Permanent)
+⁙┃ ુོ 0895-6224-13195 (Permanent)
+⁙┃ ુོ 0896-2872-6005 (Permanent)
+⁙┃ ુོ 0882-5464-100 (Permanent)
+⁙┃ ુོ 0812-6132-4817 (Permanent)
+⁙┃ ુོ 0878-3499-3772 (Permanent)
+⁙┃ ુོ 0822-6895-5211 (Permanent)
+⁙┃ ુོ 0812-2798-1614 (Permanent)
+⁙┃ ુོ 0813-8332-1456 (21 November 2021)
+⁙┃ ુོ 0853-4886-5473 (23 Oktober 2021)
+⁙┃ 
+⁙╰•°°°🕊°°°°°🕊°°°°°°🕊°°°°°°°°
+`.trim(), 'MAU IKUTAN?', 'Daftar Premium', '#sewa', m)
+handler.help = ['premlist|listprem']
+handler.tags = ['info', 'owner']
 handler.command = /^(listprem|premlist)$/i
 
 module.exports = handler
