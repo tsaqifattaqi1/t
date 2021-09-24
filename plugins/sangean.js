50ef7f44aab1a12a1c0cddc2c79671a6f80ebbb8
Irwan X Yans <irwan081249@gmail.com>
2021-09-24 22:28:50 +0700

    Update sangean.js

--- plugins/sangean.js
+++ plugins/sangean.js
@@ -1,5 +1,5 @@
 let handler = async (m) => {
- conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'Nih Jangan Sagne Bang', m)
+ conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'Nih Jangan Sagne Bang', m, 0, {mimetype: 'video/gif'})
 }
 handler.help = ['sange']
 handler.tags = ['dewasa', 'update']
