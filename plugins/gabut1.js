bd0837445cc8edfd32d879c90e14950d9da9c686
Irwan X Yans <irwan081249@gmail.com>
2021-09-25 17:50:06 +0700

    Update gabut1.js

--- plugins/gabut1.js
+++ plugins/gabut1.js
@@ -1,9 +1,10 @@
 let fetch = require("node-fetch")
 let handler = async (m, { conn }) => {
+ let id = m.chat
  let src = await (await fetch('https://raw.githubusercontent.com/irwanx/Asupan/master/gabut/gabut1.json')).json()
  let json = src[Math.floor(Math.random() * src.length)]
-
-conn.sendFile(m.chat, json, 'gabut1.jpg', m, true, { contextInfo: { forwardingScore: 999, isForwarded: true }}) 
+ let caption = `nih bang`.trim()
+        await conn.sendButton(m.chat, caption, '© XsvsH', 'Gabut 1', '.gabut1', m), json, { contextInfo: { forwardingScore: 999, isForwarded: true }}) 
 }
 
 handler.help = ['gabut1']
