49b83cb8d0848d5a10afbf9247d9270f75f5b2ae
Irwan X Yans <irwan081249@gmail.com>
2021-09-25 17:37:14 +0700

    Create gabut1.js

---
+++ plugins/gabut1.js
@@ -0,0 +1,14 @@
+let fetch = require("node-fetch")
+let handler = async (m, { conn }) => {
+ let src = await (await fetch('https://raw.githubusercontent.com/irwanx/Asupan/master/gabut/gabut1.json')).json()
+ let json = src[Math.floor(Math.random() * src.length)]
+
+conn.sendFile(m.chat, json, 'gabut1.jpg', 'Nihh', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }}) 
+}
+
+handler.help = ['gabut1']
+handler.tags = ['fun', 'update']
+handler.command = /^gabut1/i
+handler.limit = true
+
+module.exports = handler
