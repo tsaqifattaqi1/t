d7844930413d008900c3ab31609fd1836c081f3a
Irwan X Yans <irwan081249@gmail.com>
2021-09-26 00:01:38 +0700

    Update gabut3.js

--- plugins/gabut3.js
+++ plugins/gabut3.js
@@ -1,13 +1,13 @@
 let fetch = require("node-fetch")
 let handler = async (m, { conn }) => {
  let id = m.chat
- let src = await (await fetch('https://raw.githubusercontent.com/irwanx/Asupan/master/gabut/gabut3.json')).json()
+ let src = await (await fetch('https://raw.githubusercontent.com/irwanx/Asupan/master/gabut/gambar18v2.json')).json()
  let json = src[Math.floor(Math.random() * src.length)]
-        conn.sendFile(m.chat, json.url, 'gabut.jpg', 'Nihh', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+        conn.sendFile(m.chat, json.url, 'anu.jpg', 'Nihh', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 }
-handler.help = ['gabut3']
+handler.help = ['gb18']
 handler.tags = ['fun', 'update']
-handler.command = /^gabut3/i
+handler.command = /^gb18/i
 handler.limit = true
 
 module.exports = handler
