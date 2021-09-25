16098d47eb15b49fabbdabdc95aa69fc34ecb981
Irwan X Yans <irwan081249@gmail.com>
2021-09-25 20:12:22 +0700

    Update gabut2.js

--- plugins/gabut2.js
+++ plugins/gabut2.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn }) => {
  let id = m.chat
  let src = await (await fetch('https://raw.githubusercontent.com/irwanx/Asupan/master/gabut/gabut2.json')).json()
  let json = src[Math.floor(Math.random() * src.length)]
-        await conn.sendButton(m.chat, json.url, '© xsvsH', 'Gabut 1', '.gabut2', m), { contextInfo: { forwardingScore: 999, isForwarded: true }}
+        await conn.sendButton(m.chat, json.url, '© xsvsH', 'Gabut 2', '.gabut2', m), { contextInfo: { forwardingScore: 999, isForwarded: true }}
 }
 handler.help = ['gabut2']
 handler.tags = ['fun', 'update']
-handler.command = /^gabut1/i
+handler.command = /^gabut2/i
 handler.premium = true
 
 module.exports = handler
