ec728e7642421bdb9b9d8779b9eeae1ce82533d4
Irwan X Yans <irwan081249@gmail.com>
2021-09-26 01:26:28 +0700

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -8,16 +8,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
     if (!json.status) throw json
-    let { title, actors, album, img_url, mp4_url, duration } = json.result
-    let pesan = `
-Judul: ${title}
-Pemain: ${actors}
-Durasi: ${duration}
-© XsvsH
-    `.trim()
-
-    conn.sendFile(m.chat, img_url, 'eror.jpg', pesan, m, 0, { thumbnail: await (await fetch(img_url)).buffer() })
-    conn.sendFile(m.chat, mp4_url, 'error.mp4', '', m, 0, { asDocument: global.db.data.chats[m.chat].useDocument, mimetype: 'video/gif' })
+    let { url } = json.result
+  
+    conn.sendFile(m.chat, 'url.mp4', '', m, 0, { asDocument: global.db.data.chats[m.chat].useDocument, mimetype: 'video/gif' })
 
 }
 handler.help = ['playb'].map(v => v + ' <judul>')
