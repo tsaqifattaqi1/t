7d18f0fce77a6b577079f13a6c5e6b055e9823c3
Irwan X Yans <irwan081249@gmail.com>
2021-09-26 21:13:54 +0700

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,20 +1,23 @@
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. judul nya apa?\n\ncontoh:\n${usedPrefix + command} akad`
-    if (isUrl(text)) throw `uhm.. judul kak bukan pake url\n\ncontoh:\n${usedPrefix + command} handjob`
+    if (!text) throw `uhm.. judul nya apa?\n\ncontoh:\n${usedPrefix + command} blowjob`
+    if (isUrl(text)) throw `uhm.. judul kak bukan pake url\n\ncontoh:\n${usedPrefix + command} akad`
 
     let res = await fetch(global.API('raceta', '/api/porn', { search: text }))
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let json = await res.json()
     if (!json.status) throw json
-    let { url } = json.result
-  
-    conn.sendFile(m.chat, 'url.mp4', '', m, 0, { asDocument: global.db.data.chats[m.chat].useDocument, mimetype: 'video/gif' })
+    bkp = JSON.parse(JSON.stringify(res.result));
+        mmq =  bkp[Math.floor(Math.random() * bkp.length)]
+        urll = mmq.url
+        vid = await fetchJson(`https://mnazria.herokuapp.com/api/porndownloadxvideos?url=${urll}`)
+        vide = await getBuffer(vid.mp4)
 
+    sendMessage(from, vide, video )
 }
 handler.help = ['playb'].map(v => v + ' <judul>')
-handler.tags = ['downloader']
+handler.tags = ['dewasa']
 handler.command = /^playb$/i
 handler.owner = true
 
