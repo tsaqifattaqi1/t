0a03a503106aa650e55c9286b7b8b9895a68ce1c
Irwan X Yans <irwan081249@gmail.com>
2021-09-26 07:32:11 +0700

    Update okick.js

--- plugins/okick.js
+++ plugins/okick.js
@@ -1,5 +1,10 @@
 let handler = async (m, { conn, args }) => {
   let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  if(m.quoted){
+if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
+let usr = m.quoted.sender;
+conn.groupRemove(m.chat, [usr]); return;
+}
   if (!m.mentionedJid[0]) throw `tag yang mau dikick`
   let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupRemove(m.chat, [user])
