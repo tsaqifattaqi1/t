f6ebf01a052e4ab5bae106296b9a8036f8d372d7
Irwan X Yans <irwan081249@gmail.com>
2021-09-27 13:54:24 +0700

    Update anticium.js

--- plugins/anticium.js
+++ plugins/anticium.js
@@ -1,6 +1,7 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
+let users = participants.map((u) => u.jid);
 let zero = fs.readFileSync('./audio/mwah.mp3')
 conn.sendMessage(m.chat, zero, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true}, { 
 key: {
@@ -14,7 +15,7 @@ displayName: this.getName(user),
 vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;WA;;;\nFN:WA\nTEL;type=CELL;type=VOICE;waid=${user.split('@')[0]}:${user.split('@')[0]}\nEND:VCARD`
 }
 }
-}, false, { contextInfo: { mentionedJid: this.parseMention(text)}})
+})
 //conn.fakeReply(m.chat, zero, '0@s.whatsapp.net', '*🔥WOAHHHHH 🔥*', 'status@broadcast')
 }
 
